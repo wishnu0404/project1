950f3c4bacc80ab09f70cb19fcbac6917942e8ed
Vishnu Arun <vishnuarun071@gmail.com>
2025-08-01 00:39:31 +0530

    31-07-2025

--- src/test/java/testClasses/HomepagetestClass.java
+++ src/test/java/testClasses/HomepagetestClass.java
@@ -7,6 +7,8 @@ import baseClass.Baseclass;
 import pageClasses.HomePageClass;
 import pageClasses.LoginPageClass;
 import pageClasses.Login_pageClass;
+import utilities.RandomDataGeneration;
+
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
@@ -24,15 +26,23 @@ public class HomepagetestClass extends Baseclass {
 		Assert.assertEquals(actualresult, expectedresult);
 	  
   }
-  @Test
+  @Test (invocationCount = 50)
   public void verify_the_user_added_is_displayed() {
 	  lp = new LoginPageClass(driver);
 	  hp= lp.validLogin("admin", "123456");
 	  hp.clickOnendTourButton();
 	  hp.clickOntheUserManagement();
 	  hp.clickOnUsers();
+	  
+	  String firstname = RandomDataGeneration.firstName();
+	  String Prefix = RandomDataGeneration.getPrefix();
+	  String lastname = RandomDataGeneration.lastName();
+	  String email = RandomDataGeneration.generateEmail();
+	  String username = RandomDataGeneration.generateUsername();
+	  String password = RandomDataGeneration.generatePassword();
+	  
 	  hp.clickOnAddbutton();
-	  hp.fillUserAddForm("Mr", "Vishnu", "Arun", "vishnuarun071@gmail.com", "vishn071", "As@12345", "As@12345", "2");
+	  hp.fillUserAddForm(Prefix, firstname, lastname, email, username, password, password, "2");
 	  hp.checkActivecheckbox();
 	  hp.save_userFormsubmit();
 	 String actualresult= hp.checktheNameIspresnt();
