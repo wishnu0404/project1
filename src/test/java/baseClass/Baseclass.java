297c437d52f3baf1c1b0d28dd1545e41641443e6
Vishnu Arun <vishnuarun071@gmail.com>
2025-08-04 23:29:13 +0530

    04-08-2025

--- src/test/java/baseClass/Baseclass.java
+++ src/test/java/baseClass/Baseclass.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 import constants.ConstantsClass;
 
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -13,6 +14,7 @@ import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod; 
 
 public class Baseclass {
@@ -25,11 +27,20 @@ public class Baseclass {
 		FileInputStream file = new FileInputStream(ConstantsClass.filepath);
 		property.load(file);
 	}
-
-	@BeforeMethod (groups = {"tearup"})
-	public void beforeMethod() throws IOException {
+    @Parameters("Browser")
+    
+	@BeforeMethod (groups = {"tearup"}) 
+	public void beforeMethod(String browserValue) throws IOException {
 		readProperty();
-		driver = new ChromeDriver();
+		if (browserValue.equalsIgnoreCase("chrome"))
+		{
+			driver = new ChromeDriver();
+		}else if (browserValue.equalsIgnoreCase("Firefox")) {
+			driver = new FirefoxDriver();
+		}else {
+			throw new IllegalArgumentException("Browser Not Supported." + browserValue);
+		}
+		
 		driver.get(property.getProperty("Base_URL"));
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(ConstantsClass.implicitwaittimeduration));
